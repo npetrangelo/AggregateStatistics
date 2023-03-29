b3402072958059966d17dd2167c199d2845ce4b9
Nathan Petrangelo <nathan@petrangelo.org>
2023-03-29 00:00:09 -0400

    Added Sample() method

--- AggregateStatistics/PDF.cs
+++ AggregateStatistics/PDF.cs
@@ -17,4 +17,17 @@ public class PDF {
         }
     }
 
+    private static double Lerp(double a, double b, double n) {
+        return a*(n-1) + b*n;
+    }
+
+    public double Sample() {
+        var sample = new Random().NextDouble();
+        for (var i = 0; i < _integral.Length; i++) {
+            if (_integral[i] > sample) {
+                return Lerp(_min, _max, (double) i/_integral.Length);
+            }
+        }
+        return _max;
+    }
 }
