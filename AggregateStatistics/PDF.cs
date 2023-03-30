36f280d3ac6c83d4199e730060b01b1cef29a75e
Nathan Petrangelo <nathan@petrangelo.org>
2023-03-30 00:29:00 -0400

    CDF values between PDF values; still need to update Percentile()

--- AggregateStatistics/PDF.cs
+++ AggregateStatistics/PDF.cs
@@ -1,19 +1,22 @@
 ﻿namespace AggregateStatistics;
 
 public class PDF {
-    private readonly double _min, _max;
+    private readonly double _min, _max, _dx;
     private readonly double[] _pdf, _cdf;
 
     public PDF(double min, double max, double[] pdf) {
         _min = min;
         _max = max;
+        _dx = (max - min) / (pdf.Length - 1);
+        
         _pdf = pdf.Normalize(); // All PDFs must sum to 1
-        _cdf = new double[pdf.Length];
+        _cdf = new double[pdf.Length+1];
 
         var sum = 0.0;
+        _cdf[0] = sum;
         for (var i = 0; i < _pdf.Length; i++) {
             sum += _pdf[i];
-            _cdf[i] = sum;
+            _cdf[i+1] = sum;
         }
     }
 
@@ -44,10 +47,13 @@ public class PDF {
     }
 
     public new string ToString() {
-        var values = new double[_pdf.Length];
-        for (var i = 0; i < values.Length; i++) {
-            values[i] = Utils.Lerp(_min, _max, (double) i/(values.Length - 1));
+        var pdfValues = new double[_pdf.Length];
+        var cdfValues = new double[_cdf.Length];
+        for (var i = 0; i < pdfValues.Length; i++) {
+            pdfValues[i] = Utils.Lerp(_min, _max, (double) i/(pdfValues.Length - 1));
+            cdfValues[i] = pdfValues[i] - _dx / 2;
         }
-        return $"PDF={string.Join(", ", values.Zip(_pdf))} CDF={string.Join(", ", values.Zip(_cdf))}";
+        cdfValues[^1] = pdfValues[^1] + _dx / 2;
+        return $"PDF={string.Join(", ", pdfValues.Zip(_pdf))} CDF={string.Join(", ", cdfValues.Zip(_cdf))}";
     }
 }
