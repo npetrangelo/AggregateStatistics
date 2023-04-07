3d0fedd3b442a19f4390297e8727d03a8eb9226b
Nathan Petrangelo <nathan@petrangelo.org>
2023-04-06 23:46:31 -0400

    Handle signs after GCD

--- AggregateStatistics/Fraction.cs
+++ AggregateStatistics/Fraction.cs
@@ -4,11 +4,11 @@ public struct Fraction : IComparable<Fraction> {
     private readonly int _n, _d;
 
     public Fraction(int numerator, int denominator = 1) {
-        _n = numerator * Math.Sign(denominator);
-        _d = denominator * Math.Sign(denominator);
-        var gcd = Utils.GCD(_n, _d);
-        _n /= gcd;
-        _d /= gcd;
+        var gcd = Utils.GCD(numerator, denominator);
+        _n = numerator/gcd;
+        _d = denominator/gcd;
+        _n *= Math.Sign(_d);
+        _d *= Math.Sign(_d);
     }
     
     public static implicit operator double(Fraction f) => (double) f._n / f._d;
@@ -22,6 +22,7 @@ public struct Fraction : IComparable<Fraction> {
     public static Fraction operator -(Fraction a, Fraction b) => a + -b;
     public static Fraction operator *(Fraction a, Fraction b) => new (a._n*b._n, a._d*b._d);
     public static Fraction operator /(Fraction a, Fraction b) => new (a._n*b._d, a._d*b._n);
+    public static Fraction operator ^(Fraction f, int p) => new(f._n.Pow(p), f._d.Pow(p));
     
     public static bool operator >(Fraction a, Fraction b) => a._n * b._d > b._n * a._d;
     public static bool operator <(Fraction a, Fraction b) => a._n * b._d < b._n * a._d;
